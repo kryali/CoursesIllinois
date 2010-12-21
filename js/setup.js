5cac9d4fd948ec3545588639187b58fe946e8e93
Kiran Ryali <kiran.ryali@gmail.com>
2010-12-21 00:57:00 -0500

    Moved toolbar script to setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 	if(jQuery){
-		alert("Got jQuery!");
+		setupToolBar();
+		setupToolTips();
 	}
-})
+});
+
+var setupToolTips = function(){
+}
+
+var setupToolBar = function(){
+	// script to make toolbar follow user
+	$(window).scroll(function() {
+	    var navigation = $('#toolbar')[0];
+	    if (window.pageYOffset >= 713) {
+		navigation.style.position = 'fixed';
+		navigation.style.top = '0';
+	    } else {
+		navigation.style.position = 'static';
+	    }
+	});
+};
