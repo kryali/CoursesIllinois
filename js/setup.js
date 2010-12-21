322a465c746ad817686fb789ac44d009e93c56ff
Krishna <kvudata@gmail.com>
2010-12-21 10:24:02 -0500

    Fixed scrolling issue, scrollTo wasn't called with proper settings, so it defaulted to not scrolling (default duration is 0). Closes #10

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,5 @@
 var toolbarPosition = 0;
+var scroll_settings = {'axis':'y','duration':'1000'};
 
 $(document).ready(function() {
 	if(jQuery){
@@ -20,23 +21,24 @@ var setupToolBar = function(){
 	toolbarPosition = navigation.offsetTop;
 	$(window).scroll(function() {
 	    var navigation = $('#toolbar')[0];
-		if (window.pageYOffset >= toolbarPosition) {
-			navigation.style.position = 'fixed';
-			navigation.style.top = '0';
+	    if (window.pageYOffset >= toolbarPosition) {
+		    navigation.style.position = 'fixed';
+		    navigation.style.top = '0';
 	    } else {
 			navigation.style.position = 'static';
 	    }
 	});
-	console.log($('#why'));
+
+	// Set up smooth scrolling instead of jump
 	$('#what_link').click(function(){
-		$.scrollTo($('#what')[0].offsetTop);
+		$.scrollTo($('#what')[0].offsetTop, scroll_settings);
 	});
 	
 	$('#why_link').click(function(){
-		$.scrollTo($('#why')[0].offsetTop);
+		$.scrollTo($('#why')[0].offsetTop, scroll_settings);
 	});
 	
 	$('#who_link').click(function(){
-		$.scrollTo($('#who')[0].offsetTop);
+		$.scrollTo($('#who')[0].offsetTop, scroll_settings);
 	});
 };
